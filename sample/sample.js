217503f096b88666fcaa986f8ceecfc2e8f3d4ce
renjithpta <renjithkn@gmail.com>
2021-09-17 12:17:42 +0530

    Delete sample directory

--- sample/sample.js
+++
@@ -1,5 +0,0 @@
-const { writeFileSync } = require("fs");
-var parserXml = require("../lib")
-acrisData = parserXml.transformFixmToAcris('./fixmsample1.xml')
-console.log("Fixm Data : ",acrisData)
-writeFileSync('./acris.json',JSON.stringify(acrisData))
