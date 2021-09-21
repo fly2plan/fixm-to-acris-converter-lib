48329734df946f745a37f42e58433cd33834c883
renjithpta <renjithkn@gmail.com>
2021-09-21 09:57:16 +0530

    Update sample.js

--- sample/sample.js
+++ sample/sample.js
@@ -1,12 +1,5 @@
 const { writeFileSync} = require('fs')
-var parser = require('../lib/index')
-
-
-// let xmlString = readFileSync('/home/sethuraman/Documents/Workspace/IBS/DataMapping/Adapter/fixm-to-acris-converter-lib/sample/fixmsample1.xml')
-
+let parser = require('../lib/index')
 let acrisData  = parser.transformXmlFileToAcris('./4.2/fixmorginal.xml')
-
-// console.log(ast)
-
 console.log("Data : ",acrisData)
 writeFileSync("acris.json",JSON.stringify(acrisData))
