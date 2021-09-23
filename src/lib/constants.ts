aba819b8ff64c335c3aa34248b65eed35d8e9d3a
renjithpta <renjithkn@gmail.com>
2021-09-23 11:10:07 +0530

    Delete src directory

--- src/lib/constants.ts
+++
@@ -1,4 +0,0 @@
-export const ICAO_KEYS = ["locationIndicator"]
-export const TAGS_REMOVABLE =["fx:","fb:"]
-export const KEYS_REMOVABLE = ["extension","$","xsi:type"]
-export const DEFAULT_VERSION = "4.2"
