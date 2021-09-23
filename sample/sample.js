69f63d41ef0e6f8ca3928f3b67ab36cdaf072626
renjithpta <renjithkn@gmail.com>
2021-09-23 11:09:54 +0530

    Delete sample directory

--- sample/sample.js
+++
@@ -1,5 +0,0 @@
-const { writeFileSync} = require('fs')
-let parser = require('../lib/index')
-let acrisData  = parser.transformXmlFileToAcris('./4.2/fixmorginal.xml')
-console.log("Data : ",acrisData)
-writeFileSync("acris.json",JSON.stringify(acrisData))
--- sample/samplefixmlib.js
+++
@@ -1,6 +0,0 @@
-const { writeFileSync,readFileSync} = require('fs');
-var parser = require('../lib')
-let fileContent = readFileSync('./4.2/fixmorginal.xml',"utf8");
-let acrisData  = parser.transformFixmToAcris(fileContent);
-console.log("Data : ",acrisData);
-writeFileSync("acris.json",JSON.stringify(acrisData));
