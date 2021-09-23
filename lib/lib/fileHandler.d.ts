423d5bfef0e70a732f9d46f65722ac47d5546e8f
renjithpta <renjithkn@gmail.com>
2021-09-23 11:09:43 +0530

    Delete lib directory

--- lib/lib/fileHandler.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const writeFile: (fileName: string, data: any) => void;
-export declare const readFile: (filepath: string) => string;
--- lib/lib/parser.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const transformXmlFileToAcris: (fixmFilePath: string, fixmversion?: string) => any;
-export declare const transformFixmToAcris: (xmlString: any, fixmversion?: string) => any;
