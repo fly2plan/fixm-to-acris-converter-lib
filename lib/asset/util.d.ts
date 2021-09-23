465201f852622cc0cbd4ec96a1f014fd475f7712
Ubuntu <ubuntu@ip-10-251-4-67.ap-south-1.compute.internal>
2021-09-23 05:44:58 +0000

    Updated after review

---
+++ lib/asset/util.d.ts
@@ -0,0 +1,4 @@
+export declare const getModel: (modelFileName: string, version?: string) => string;
+export declare const getIATA: () => any;
+export declare const getAlternative: () => any;
+export declare const getCollectionModel: (version?: string) => any;
