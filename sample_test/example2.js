c0f49adeac162efc674dea3999142cf33928ee9f
renjithpta <renjithkn@gmail.com>
2021-10-01 16:28:47 +0530

    Delete sample_test directory

--- sample_test/example2.js
+++
@@ -1,6 +0,0 @@
-const { writeFileSync, readFileSync} = require('fs');
-let parser = require('../lib/index');
-let fixmData = readFileSync('./fixminput/fixmorginal.xml',"utf8");
-let acrisData  = parser.transformFixmToAcris(fixmData);
-console.log("Data : ",acrisData);
-writeFileSync("./output/acris_example2.json",JSON.stringify(acrisData));
